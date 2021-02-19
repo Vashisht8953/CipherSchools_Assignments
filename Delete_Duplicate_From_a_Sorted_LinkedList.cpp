d9ae19ad8375b2969d962b10fd29d8888756661d
Vashisht8953 <63307973+Vashisht8953@users.noreply.github.com>
2021-02-19 21:40:48 +0530

    Add files via upload

---
+++ Delete_Duplicate_From_a_Sorted_LinkedList.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node 
+{ 
+	int data; 
+	struct Node *next; 
+}; 
+ 
+struct Node *newNode(int data) 
+{ 
+Node *temp = new Node; 
+temp -> data = data; 
+temp -> next = NULL; 
+return temp; 
+} 
+
+void printList(struct Node *node) 
+{ 
+	while (node != NULL) 
+	{ 
+		cout << node -> data << "-> "; 
+		node = node -> next; 
+	} 
+} 
+ 
+void removeAllDuplicates(struct Node* &start) 
+{ 
+	Node* dummy = new Node; 
+
+	dummy -> next = start; 
+
+	Node* prev = dummy; 
+ 
+	Node* current = start; 
+
+	while(current != NULL) 
+	{ 
+		while(current -> next != NULL && 
+			prev -> next -> data == current -> next -> data) 
+			current = current -> next; 
+
+		if (prev -> next == current) 
+			prev = prev -> next; 
+
+		else
+			prev -> next = current -> next; 
+
+		current = current -> next; 
+	} 
+
+	start = dummy -> next; 
+} 
+
+int main() 
+{ 
+	struct Node* start = newNode(1); 
+	start -> next = newNode(2); 
+	start -> next -> next = newNode(2); 
+	start -> next -> 
+	next -> next = newNode(3); 
+	start -> next -> 
+	next -> next -> next = newNode(4); 
+	start -> next -> 
+	next -> next -> 
+	next -> next = newNode(4); 
+	start -> next -> 
+	next -> next -> 
+	next -> next -> next = newNode(5); 
+	start -> next -> 
+	next -> next -> 
+	next -> next -> 
+	next -> next = newNode(5); 
+	cout << "List before removal " << "of duplicates : " <<" "; 
+	printList(start); 
+	
+	removeAllDuplicates(start); 
+	
+	cout << " \nList after removal " << "of duplicates : " << " "; 
+	printList(start); 
+	return 0; 
+} 
+
