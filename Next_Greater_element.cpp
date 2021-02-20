0c5f5480580459a371411fa7d087b4ccabe05bcc
Vashisht8953 <63307973+Vashisht8953@users.noreply.github.com>
2021-02-20 18:19:09 +0530

    Add files via upload

---
+++ Next_Greater_element.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNGE(int arr[], int n)
+{
+	stack<int> s;
+
+	s.push(arr[0]);
+
+	for (int i = 1; i < n; i++) 
+	{
+
+		if (s.empty()) {
+			s.push(arr[i]);
+			continue;
+		}
+
+		while (s.empty() == false
+			&& s.top() < arr[i]) 
+		{
+			cout << s.top() 
+				<< " --> " << arr[i] << endl;
+			s.pop();
+		}
+
+		s.push(arr[i]);
+	}
+	
+	while (s.empty() == false) {
+		cout << s.top() << " --> " << -1 << endl;
+		s.pop();
+	}
+}
+
+int main()
+{
+	int arr[] = { 17, 14, 21, 3 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	printNGE(arr, n);
+	return 0;
+}
+
